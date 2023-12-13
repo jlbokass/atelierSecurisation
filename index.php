d59e57bcfabfe4d9407dfffea178f1ce2df8ad2b
Jean le Grand Bokassa <jean.le.grand.bokassa@gmail.com>
2023-12-13 13:48:40 +0100

    wip

--- index.php
+++ index.php
@@ -1,3 +1,74 @@
+<?php
+// Initializing variables
+$firstname = "";
+$lastname = "";
+$password = "";
+$email = "";
+$phone = "";
+$description = "";
+$errors = [];
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+// Récupération des données du formulaire
+    $firstname = $_POST["firstname"];
+    $lastname = $_POST["lastname"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $description = $_POST["description"];
+
+    // Validate firstname
+    if($firstname === '') {
+        $errors[] = "Firstname can't be empty";
+    }
+
+    // Validate lastname
+    if($lastname === '') {
+        $errors[] = "Lastname can't be empty";
+    }
+
+    // Validate password
+    if($password === '') {
+        $errors[] = "Lastname can't be empty";
+    }
+
+    // Validate email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Veuillez saisir une adresse e-mail valide.";
+    }
+
+    // Validate phone number
+    if (!preg_match("/^[0-9]{10}$/", $phone)) {
+        $errors[] = "Le numéro de téléphone doit contenir 10 chiffres.";
+    }
+
+    // Validate lastname
+    if($description === '') {
+        $errors[] = "Description can't be empty";
+    }
+
+    // if no errors
+    if (empty($errors)) {
+        $servername = "localhost";
+        $username = "root";
+        $password = "dabok1977";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+            echo "Connected successfully";
+        }
+
+        // Insert data into database
+        /*$sql = "INSERT INTO `security_workshop`.`as-users` (`firstname`, `lastname`, `password`, `email`, `phone`, `description`) VALUES ('$firstname', '$lastname', '$password', '$email', '$phone', '$description')";*/
+
+        $conn->close();
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +80,19 @@
 </head>
 <body>
 <div class="container">
-    <form method="post" id="form" class="form">
+    <?php if (! empty($errors)) : ?>
+        <ul>
+            <?php foreach ($errors as $error) : ?>
+                <li><?= $error ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+    <form
+            method="post"
+            id="form"
+            class="form"
+            action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+    >
         <h2>User Registration Form</h2>
         <!-- Firstname -->
         <div class="form-control">
@@ -19,6 +102,7 @@
                     id="firstname"
                     name="firstname"
                     placeholder="Enter your firstname"
+                    value="<?php echo $firstname;?>"
             >
             <small>Error message</small>
         </div>
@@ -30,6 +114,7 @@
                     id="lastname"
                     name="lastname"
                     placeholder="Enter your lastname"
+                    value="<?php echo $lastname;?>"
             >
             <small>Error message</small>
         </div>
@@ -53,6 +138,7 @@
                     id="email"
                     name="email"
                     placeholder="Enter your email"
+                    value="<?php echo $email;?>"
             >
             <small>Error message</small>
         </div>
@@ -65,6 +151,7 @@
                     id="phone"
                     name="phone"
                     placeholder="Enter your phone number"
+                    value="<?php echo $phone;?>"
             >
             <small>Error message</small>
         </div>
@@ -78,7 +165,7 @@
                     rows="4"
                     cols="50"
                     placeholder="Enter your description"
-            ></textarea>
+            ><?php echo $description;?></textarea>
         </div>
         <!-- Submit button -->
         <button type="submit">Submit</button>
